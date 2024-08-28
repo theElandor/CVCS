4081ad85824596c90591c55985c7f5d5e980bed7
nicola <nicolamorelli30008@gmail.com>
2024-08-28 10:34:52 +0200

    fixes

--- source/scripts/evaluation.py
+++ source/scripts/evaluation.py
@@ -18,7 +18,7 @@ except:
 if 'load_checkpoint' in config:
     utils.load_checkpoint(config, net)
 # chunk size of 1 for validation and no random shift
-loader = dataset.Loader(config['dataset'], 10, patch_size=config['patch_size'])
+loader = dataset.Loader(config['dataset'], 1, patch_size=config['patch_size'])
 if 'images' in config.keys():
     loader.specify(config['images'])
 flat, normalized = utils.eval_model(net, 
